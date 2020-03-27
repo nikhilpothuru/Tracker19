2037677df9bf9f733b1841a77973ee301c9ee237
Nikhil Pothuru <nikhilpothuru@Nikhils-MacBook-Pro-3.local>
2020-03-26 23:41:55 -0700

    Created CDC Scrapper File

--- dataScrapping/cdcScrapper.py
+++ dataScrapping/cdcScrapper.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
+# Grab the url for the CDC data
 my_url = 'https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html'
 uClient = uReq(my_url)
 
