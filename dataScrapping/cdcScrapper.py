b0989fa5ec74362c1f339de71becb5c9fc5db6ca
Nikhil Pothuru <nikhilpothuru@Nikhils-MacBook-Pro-3.local>
2020-04-07 23:52:50 -0700

    remove cdc

--- dataScrapping/cdcScrapper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as uReq
-
-# Grab the url for the CDC data
-my_url = 'https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html'
-uClient = uReq(my_url)
-
-#match = soup.find()
